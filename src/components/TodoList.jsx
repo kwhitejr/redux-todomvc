2f743db092b1d5627c59d0a43e0e0ce5ce4db7a2
kwhitejr <kwhitejr.edu@gmail.com>
2016-06-05 12:03:04 -1000

    added filter function to TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,10 +9,19 @@ export default class TodoList extends React.Component {
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
   }
 
+  getItems() {
+    if (this.props.todos) {
+      return this.props.todos.filter(
+        (item) => item.get('status') === this.props.filter
+      );
+    }
+    return [];
+  }
+
   render() {
     return <section className="main">
       <ul className="todo-list">
-        {this.props.todos.map(item =>
+        {this.getItems().map(item =>
           <TodoItem key={item.get('text')}
                     text={item.get('text')} />
         )}
