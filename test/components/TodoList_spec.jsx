a05b5a609164a95462338b3c1d04780a994ea797
kwhitejr <kwhitejr.edu@gmail.com>
2016-06-05 11:59:19 -1000

    added test for Todolist

---
+++ test/components/TodoList_spec.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import TodoList from '../../src/components/TodoList';
+import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
+const {renderIntoDocument,
+      scryRenderedDOMComponentsWithTag} = TestUtils;
+
+describe('TodoList', () => {
+
+  it('renders a list with only the active items if the filer is active', () => {
+    const todos = List.of(
+      Map({id: 1, text: 'React', status: 'active'}),
+      Map({id: 2, text: 'Redux', status: 'active'}),
+      Map({id: 3, text: 'Immutable', status: 'completed'})
+    );
+    const filter = 'active';
+    const component = renderIntoDocument(
+      <TodoList filter={filter} todos={todos} />
+    );
+    const items = scryRenderedDOMComponentsWithTag(component, 'li');
+
+    expect(items.length).to.equal(2);
+    expect(items[0].textContent).to.contain('React');
+    expect(items[1].textContent).to.contain('Redux');
+  })
+})
