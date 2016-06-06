ec64c2d5a13e4e97dd98e6a2c24f2c0248bceda8
kwhitejr <kwhitejr.edu@gmail.com>
2016-06-05 14:06:45 -1000

    added functionality and tests for TodoItem

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,29 +3,33 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 import TodoItem from './TodoItem';
 
-export default class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-  }
+export default React.createClass({
+  mixins: [PureRenderMixin],
 
-  getItems() {
+  getItems: function () {
     if (this.props.todos) {
       return this.props.todos.filter(
-        (item) => this.props.filter === 'all' || item.get('status') === this.props.filter
+        (item) => this.props.filter === 'all' ||
+          item.get('status') === this.props.filter
       );
     }
     return [];
-  }
+  },
 
-  render() {
+  isCompleted: function(item) {
+    return item.get('status') === 'completed';
+  },
+
+  render: function () {
     return <section className="main">
       <ul className="todo-list">
         {this.getItems().map(item =>
           <TodoItem key={item.get('text')}
-                    text={item.get('text')} />
+                    text={item.get('text')}
+                    isCompleted={this.isCompleted(item)}
+                    isEditing={item.get('editing')} />
         )}
       </ul>
     </section>
   }
-};
+});
